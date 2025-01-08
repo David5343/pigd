63c2e6fcb517b729a9707c384c419cd44d5984aa
David <60202438+David5343@users.noreply.github.com>
2025-01-08 00:38:47 -0600

    agregando ruta para cmedica en route web

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::get('cmedica',function (){
+        return view('cmedica.index');
+    })->name('cmedica');
 });
