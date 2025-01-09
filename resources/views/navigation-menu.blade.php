ecc17167661e3d0b3f0f9513a7c7d859c5c3d2c7
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-01-09 12:13:55 -0600

    agregando link en menu de navegacion en material resources

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -41,7 +41,7 @@
                         {{ __('Recursos Financieros') }}
                     </x-nav-link>
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        <x-nav-link href="{{ route('mresources') }}" :active="request()->routeIs('mresources')">
                             {{ __('Recursos Materiales') }}
                         </x-nav-link>
                     </div>
