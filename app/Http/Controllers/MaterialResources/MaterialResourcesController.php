43ca28902505b0b54c3fd4d8da33f5eb165331b9
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-01-09 12:12:52 -0600

    agregar funcion index en controlador material resources

--- app/Http/Controllers/MaterialResources/MaterialResourcesController.php
+++ app/Http/Controllers/MaterialResources/MaterialResourcesController.php
@@ -6,5 +6,9 @@ use App\Http\Controllers\Controller;
 
 class MaterialResourcesController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('material_resources.index');
+
+    }
 }
