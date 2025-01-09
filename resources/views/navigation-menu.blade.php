aca159c48ccdba29bdc6e30b0436b2065671bd77
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-01-09 12:06:03 -0600

    agregando link a menu de navegacion de financial resources

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -37,7 +37,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-nav-link href="{{ route('fresources') }}" :active="request()->routeIs('fresources')">
                         {{ __('Recursos Financieros') }}
                     </x-nav-link>
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
