3286d59131f11c2a46f4dccc0a29ff988e4527bc
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-01-09 11:55:24 -0600

    agregando ruta para index en human resources

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\GeneralAdministration\GeneralAdministrationController;
 use App\Http\Controllers\GeneralCoordination\GeneralCoordinationController;
+use App\Http\Controllers\HumanResources\HumanResourcesController;
 use App\Http\Controllers\LegalDepartment\LegalDepartmentController;
 use App\Http\Controllers\MedicalCoordination\MedicalCoordinationController;
 use Illuminate\Support\Facades\Route;
@@ -23,4 +24,5 @@ Route::middleware([
     Route::get('/general_administration', [GeneralAdministrationController::class, 'index'])->name('gadministration');
     Route::get('/legal_department', [LegalDepartmentController::class, 'index'])->name('ldepartment');
     Route::get('/medical_coordination', [MedicalCoordinationController::class, 'index'])->name('mcoordination');
+    Route::get('/human_resources', [HumanResourcesController::class, 'index'])->name('hresources');
 });
