f3241eac94a20007a76996a55d3354ee63b4daed
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-01-09 10:10:37 -0600

    actualizando vista de navegacion ruta general coordination

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-nav-link href="{{ route('gcoordination') }}" :active="request()->routeIs('gcoordination')">
                         {{ __('Coordinación General') }}
                     </x-nav-link>
                 </div>
