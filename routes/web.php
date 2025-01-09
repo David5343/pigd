931a8761c8da235bc7609421ad001a496bc9f42a
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-01-09 11:37:32 -0600

    agregando ruta para index en legal department

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\GeneralAdministration\GeneralAdministrationController;
 use App\Http\Controllers\GeneralCoordination\GeneralCoordinationController;
+use App\Http\Controllers\LegalDepartment\LegalDepartmentController;
 use App\Http\Controllers\MedicalCoordination\MedicalCoordinationController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,5 @@ Route::middleware([
     Route::get('cmedica', [MedicalCoordinationController::class, 'index'])->name('cmedica');
     Route::get('/general_coordination', [GeneralCoordinationController::class, 'index'])->name('gcoordination');
     Route::get('/general_administration', [GeneralAdministrationController::class, 'index'])->name('gadministration');
+    Route::get('/legal_department', [LegalDepartmentController::class, 'index'])->name('ldepartment');
 });
