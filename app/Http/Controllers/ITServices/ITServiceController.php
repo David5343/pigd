070b6b0cb7f9ceca79dbd7a73d1f376179e7a23b
David <60202438+David5343@users.noreply.github.com>
2025-01-12 11:52:02 -0600

    creando controladores para tecnologias,transparencia ,usuarios y archivo

---
+++ app/Http/Controllers/ITServices/ITServiceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ITServices;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ITServiceController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Transparency/TransparencyController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Transparency;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TransparencyController extends Controller
+{
+    //
+}
