89f18c8196020f309ed1f6030f400f9c5cd532a3
David <60202438+David5343@users.noreply.github.com>
2025-01-12 11:38:51 -0600

    creando vista index de controlador para general administration donde se llama componente submenu

--- resources/views/general_administration/index.blade.php
+++ resources/views/general_administration/index.blade.php
@@ -1,14 +1,51 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{-- <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Administración General') }}
-        </h2>
+        </h2> --}}
+        @livewire('general-administration.sub-menu-administration')
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 {{-- <x-welcome /> --}}
+                <div>
+
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum esse numquam optio nemo alias placeat
+                    modi, earum necessitatibus hic quibusdam voluptatem ipsam atque explicabo laudantium id, corporis
+                    nihil eaque reiciendis!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quod quaerat soluta fugit voluptate
+                    reiciendis ipsa sint impedit ad eligendi. Maiores, ducimus. Illo itaque dolorem saepe nemo inventore
+                    laudantium placeat.
+                    Accusamus laborum aliquam molestias, velit explicabo soluta dicta nihil nostrum officia ea possimus
+                    aut rerum sequi pariatur voluptatum quasi esse consectetur aliquid quisquam magni incidunt saepe
+                    facilis? Eligendi, deserunt dolor.
+                    Iusto tenetur cupiditate, officia obcaecati aut perferendis reiciendis soluta magni nesciunt ipsam.
+                    Alias voluptate ipsum, nesciunt debitis esse fugit quam eveniet aperiam quod quo deserunt magnam,
+                    aspernatur quaerat voluptatum a.
+                    In, cum. Qui odit illum itaque natus, veniam ipsum ut fugiat excepturi iusto, atque quidem
+                    voluptatem voluptatibus minima consequuntur eos cupiditate labore et modi dolorem eligendi aperiam
+                    adipisci alias quaerat.
+                    Deleniti soluta animi quas impedit. Culpa enim dolorem nemo vero, distinctio magni iste sapiente non
+                    cumque aut, ut aspernatur beatae id, sequi voluptate quam error consequatur sed nisi quibusdam
+                    totam.
+                    Dolores, cupiditate ut ex accusantium nihil molestias expedita. Nobis ipsam officia consequatur
+                    doloribus perferendis. Ex aperiam suscipit et accusamus, accusantium reprehenderit, veniam rem
+                    asperiores vel hic nesciunt voluptatibus, dolor itaque.
+                    Commodi, eligendi quia soluta, dolores in excepturi id mollitia dolore eaque, neque doloremque
+                    laboriosam consequatur! Ipsa inventore, fugit tempora impedit delectus, accusamus magni repudiandae
+                    sit neque atque ea, molestias aliquam?
+                    Laudantium mollitia aliquid repellat ullam sit autem, excepturi, dolores libero suscipit facilis
+                    deserunt fugiat aspernatur sequi quod ipsa odio dignissimos et assumenda fugit accusamus soluta
+                    saepe sunt praesentium! Deserunt, ipsum.
+                    Commodi optio tempora ipsam vel odit minima culpa ducimus, non sunt quibusdam sed minus repellendus
+                    eveniet mollitia necessitatibus unde, enim inventore totam ad fuga! Vel sint labore unde cum
+                    officia.
+                    Praesentium rerum illo corporis adipisci obcaecati vel odit quis culpa nihil nulla autem tenetur
+                    libero in fugit reprehenderit beatae qui molestiae ipsa laudantium, fugiat pariatur ut, ab provident
+                    veniam! At.
+                </div>
             </div>
         </div>
     </div>
