c35361dda188193f35e0785e2f489e7067518826
David <60202438+David5343@users.noreply.github.com>
2025-01-13 01:05:34 -0600

    agregando la vista de usuarios

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        {{-- <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Administración General') }}
+        </h2> --}}
+        @livewire('general-administration.sub-menu-administration')
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                {{-- <x-welcome /> --}}
+                <div>
+                    Esta es la vista index de usuarios.
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
