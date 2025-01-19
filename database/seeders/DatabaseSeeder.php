b9b4aa59fc5c19ae712d9fdc9a07f34dd89815b2
David <60202438+David5343@users.noreply.github.com>
2025-01-19 16:38:09 -0600

    agregando el seede de api token a la ejecucion  de todos los seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call([UserSeeder::class,RoleSeeder::class]);
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+            ApiTokenUserSeeder::class,
+        ]);
     }
 }
