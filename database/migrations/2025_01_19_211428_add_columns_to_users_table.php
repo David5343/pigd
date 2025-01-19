b3d255bf61f5dd04953a886833342f3842dd6210
David <60202438+David5343@users.noreply.github.com>
2025-01-19 15:22:08 -0600

    creando migracion para agregar soft deletes,api_token y modified_by

---
+++ database/migrations/2025_01_19_211428_add_columns_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('modified_by', 255)->nullable();
+            $table->string('api_token', 255)->nullable()->after('remember_token');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('modified_by');
+            $table->dropColumn('api_token');
+            $table->dropSoftDeletes();
+        });
+    }
+};
