4bb7bf6b52f0b54e8e6816fa0a031e6343d657c0
David <60202438+David5343@users.noreply.github.com>
2025-01-19 16:36:57 -0600

    agregando ruta para el login de la api

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\API\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/login', [ApiController::class, 'login']);
+Route::get('/users', [ApiController::class, 'index']);
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/users', [ApiController::class, 'index']);
