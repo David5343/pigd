f6811ede35b110dd48deded4db9fc8072f89ec89
David <60202438+David5343@users.noreply.github.com>
2025-01-19 12:30:05 -0600

    creando seeder para los usuarios admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'name' => 'David Sánchez de la Cruz',
+                'email' => 'david@pigd.com',
+                'password' => bcrypt('david2023'),
+            ],
+            [
+                'name' => 'Luis Adelfo Roblez Vazquez',
+                'email' => 'adelfo@pigd.com',
+                'password' => bcrypt('adelfo2023'),
+            ],
+        ];
+
+        foreach ($users as $userData) {
+            User::create($userData);
+        }
+
+    }
+}
