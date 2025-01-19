3d788e3a9fb6c70f98e9fbd7fac01f3c4a3d7ae5
David <60202438+David5343@users.noreply.github.com>
2025-01-19 12:30:40 -0600

    creando seeder para los roles del sistema

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Lista de nombres de roles
+        $roles = [
+            'CoordinacionGeneral',
+            'AdministracionGeneral',
+            'AreaJuridica',
+            'CoordinacionMedica',
+            'RecursosHumanos',
+            'RecursosFinancieros',
+            'RecursosoMateriales',
+            'PrestacionesSocioEcnomicas',
+            'Tecnologias',
+            'JefaturaCoordinacion',
+            'JefaturaAdministracion',
+            'JefaturaJuridica',
+            'JefaturaMedica',
+            'JefaturaHumanos',
+            'JefaturaFinancieros',
+            'JefaturaMateriales',
+            'JefaturaPrestaciones',
+            'JefaturaTecnologias',
+            'Admin',
+            'Default',
+        ];
+
+        // Crear cada rol en la base de datos
+        foreach ($roles as $roleName) {
+            Role::firstOrCreate(['name' => $roleName]);
+        }
+
+    }
+}
