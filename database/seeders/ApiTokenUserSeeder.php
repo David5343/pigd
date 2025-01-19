d901c09704b4e407620913a68d5a29e19492d67e
David <60202438+David5343@users.noreply.github.com>
2025-01-19 16:37:37 -0600

    creando seeder de creacion de apitoken para usuarios

---
+++ database/seeders/ApiTokenUserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ApiTokenUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userEmails = ['david@pigd.com', 'adelfo@pigd.com'];
+        foreach ($userEmails as $email) {
+            $user = User::where('email', $email)->first();
+            if ($user) {
+                $this->refreshApiToken($user);
+            }
+        }
+    }
+    private function refreshApiToken(User $user)
+    {
+        if ($user->api_token === null) {
+            // Crear un nuevo token
+            $token = $user->createToken($user->email);
+            $user->api_token = $token->plainTextToken;
+        } else {
+            // Eliminar el token existente y crear uno nuevo
+            $user->tokens()->where('name', $user->email)->delete();
+            $token = $user->createToken($user->email);
+            $user->api_token = $token->plainTextToken;
+        }
+
+        $user->modified_by = 'david@pigd.com';
+        $user->save();
+    }
+}
