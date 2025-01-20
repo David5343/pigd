85693346f908587fda765c39c771205b45d399bc
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-01-20 13:27:16 -0600

    creando migracion de tabla bancos

---
+++ database/migrations/2025_01_20_192150_create_banks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banks', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+            $table->id();
+            $table->string('key', 255)->unique();
+            $table->string('name', 255);
+            $table->string('legal_name', 255);
+            $table->enum('status', ['active', 'inactive', 'deleted']);
+            $table->string('modified_by', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banks');
+    }
+};
