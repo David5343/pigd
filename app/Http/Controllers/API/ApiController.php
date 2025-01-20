c8459f3a6e3a84a38aa48b99a57e9dc561571cdd
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-01-20 11:21:03 -0600

    prueba de logeo en nuevo servidor y nueva version de pigd

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -21,38 +21,30 @@ class ApiController extends Controller
 
     public function login(Request $request)
     {
-        $response = [
-            'success' => true,
-            'message' => null,
-            'data' => null,
-            'errors' => null,
-        ];
-        // Validar los datos
-        $validatedData = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required|string|min:8',
-        ]);
-        $validator = Validator::make($request->all(), $validatedData);
-        // Comprobar si la validación falla
-        if ($validator->fails()) {
-            // Retornar errores de validación
-            $response['errors'] = $validator->errors()->toArray();
+        $response = ['status' => 0,
+            'msg' => ''];
+        $data = json_decode($request->getContent());
+        if (isset($data->email)) {
+            $user = User::where('email', $data->email)->first();
+            if ($user) {
+                if (Hash::check($data->password, $user->password)) {
+                    //$token = $user->createToken($data->email);
+                    $response['status'] = 1;
+                    //$response["token"] = $token->plainTextToken;
+                    $response['msg'] = 'Inicio de Session exitoso.';
+                    $response['user'] = $user;
+                } else {
+                    $response['msg'] = 'Estas Credenciales no coinciden con nuestros registros.';
+                }
+            } else {
+                $response['msg'] = 'Usuario no encontrado.';
+            }
 
-            // $response['debug'] = $request->all();
-            return response()->json($response, 200);
+        } else {
+            $response['msg'] = 'Ingrese Parametros validos.';
         }
-        // Buscar usuario
-        $user = User::where('email', $validatedData['email'])->first();
-        if (! $user || ! Hash::check($validatedData['password'], $user->password)) {
-            $response['success'] = false;
-            $response['message'] = 'Credenciales Inválidas.';
 
-            return response()->json([$response], 401);
-        }
-        $response['message'] = 'Login successful.';
-        $response['data'] = $user;
-
-        return response()->json($response, 200);
+        return response()->json($response);
     }
 
     public function logout(Request $request)
