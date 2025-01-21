eaf88e5a629c16661138dfc461bb5566c274ae24
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-01-21 14:27:00 -0600

    creando controlador de titulares para el api

---
+++ app/Http/Controllers/API/SocioeconomicBenefits/CredentialInsuredApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\SocioeconomicBenefits;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CredentialInsuredApiController extends Controller
+{
+    //
+}
