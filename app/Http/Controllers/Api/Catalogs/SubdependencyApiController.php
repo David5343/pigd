8c802f488ebe0d58a9feb031daa571e2caa206b1
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-01-22 13:31:46 -0600

    agregando controladores de catalogos para api

---
+++ app/Http/Controllers/Api/Catalogs/SubdependencyApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Catalogs;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SubdependencyApiController extends Controller
+{
+    //
+}
