6360bfe5c23438e3d2a4d9aeda2f679c2f1c5826
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-01-29 09:19:47 -0600

    ajuste en frecuencia de ejecucion de tarea

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ use Illuminate\Support\Facades\Schedule;
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(BackupDatabase::class)->dailyAt('19:00')->runInBackground();
+Schedule::command(BackupDatabase::class)->dailyAt('19:00');
