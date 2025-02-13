be108c413f0e4ead621fc45da67fdf2bdf7fce23
David <60202438+David5343@users.noreply.github.com>
2025-02-12 22:14:44 -0600

    agregando submenu de coordinacion medica

--- resources/views/medical_coordination/index.blade.php
+++ resources/views/medical_coordination/index.blade.php
@@ -1,14 +1,21 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Coordinación Médica') }}
-        </h2>
+        @livewire('medical-coordination.sub-menu-medical-coordination')
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 {{-- <x-welcome /> --}}
+                <div>
+
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum esse numquam optio nemo alias placeat
+                    modi, earum necessitatibus hic quibusdam voluptatem ipsam atque explicabo laudantium id, corporis
+                    nihil eaque reiciendis!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quod quaerat soluta fugit voluptate
+                    reiciendis ipsa sint impedit ad eligendi. Maiores, ducimus. Illo itaque dolorem saepe nemo inventore
+                    laudantium placeat.
+                </div>
             </div>
         </div>
     </div>
