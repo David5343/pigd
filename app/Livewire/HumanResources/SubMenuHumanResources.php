780533904462ede1d42598bdece5e32f103e62e3
David <60202438+David5343@users.noreply.github.com>
2025-02-12 22:25:33 -0600

    agregando menu a recursos humanos

---
+++ app/Livewire/HumanResources/SubMenuHumanResources.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Livewire\HumanResources;
+
+use Livewire\Component;
+
+class SubMenuHumanResources extends Component
+{
+    public $isSubmenu1Visible = false;
+    public $isSubmenu2Visible = false;
+    public $isSubmenu3Visible = false;
+
+    public function toggleSubmenu($submenu)
+    {
+        if ($submenu === 'submenu1') {
+            $this->isSubmenu1Visible = !$this->isSubmenu1Visible;
+        } elseif ($submenu === 'submenu2') {
+            $this->isSubmenu2Visible = !$this->isSubmenu2Visible;
+        } elseif ($submenu === 'submenu3') {
+            $this->isSubmenu3Visible = !$this->isSubmenu3Visible;
+        }
+    }
+    private function resetVisibility()
+    {
+        $this->isSubmenu1Visible = false;
+        $this->isSubmenu2Visible = false;
+        $this->isSubmenu3Visible = false;
+    }
+    public function render()
+    {
+        return view('livewire.human-resources.sub-menu-human-resources');
+    }
+}
