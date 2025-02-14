0e90c28204bb7849c22168600432dc66f2fecf6d
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-02-14 15:44:03 -0600

    agregar submenu de recursos materiales

--- resources/views/material_resources/index.blade.php
+++ resources/views/material_resources/index.blade.php
@@ -1,14 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Recursos Materiales') }}
-        </h2>
+        {{-- <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Administración General') }}
+        </h2> --}}
+        @livewire('material_resources.sub-menu-material-resources')
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 {{-- <x-welcome /> --}}
+                <div>
+
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum esse numquam optio nemo alias placeat
+                    modi, earum necessitatibus hic quibusdam voluptatem ipsam atque explicabo laudantium id, corporis
+                    nihil eaque reiciendis!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quod quaerat soluta fugit voluptate
+                    reiciendis ipsa sint impedit ad eligendi. Maiores, ducimus. Illo itaque dolorem saepe nemo inventore
+                    laudantium placeat.
+                </div>
             </div>
         </div>
     </div>
