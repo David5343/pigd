7565d7a0ab63337b7fee63359ca0493cac36be05
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-02-18 15:01:19 -0600

    eliminado vista que no se usara y resetenacion vista index de cada area

--- resources/views/socioeconomic_benefits/index.blade.php
+++ resources/views/socioeconomic_benefits/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        {{-- <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Administración General') }}
-        </h2> --}}
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Prestaciones SocioEconómicas') }}
+        </h2>
     </x-slot>
 </x-app-layout>
--- resources/views/socioeconomic_benefits/membership-menu.blade.php
+++
@@ -1,9 +0,0 @@
-<x-app-layout>
-    <x-slot name="header">
-        {{-- <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Administración General') }}
-        </h2> --}}
-        @livewire('socioeconomic-benefits.sub-menu-socioeconomic-benefits')
-    </x-slot>
-    @livewire('socioeconomic-benefits.sub-menu-membership')
-</x-app-layout>
