5328709027a3b1c085a6e97d65407cc60a07f9fe
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-02-17 18:39:26 -0600

    agregand submenu de afiliacion en prestaciones

---
+++ app/Livewire/SocioeconomicBenefits/SubMenuMembership.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\SocioeconomicBenefits;
+
+use Livewire\Component;
+
+class SubMenuMembership extends Component
+{
+
+    public function render()
+    {
+        return view('livewire.socioeconomic-benefits.sub-menu-membership');
+    }
+}
---
+++ resources/views/livewire/socioeconomic-benefits/sub-menu-membership.blade.php
@@ -0,0 +1,24 @@
+    <div class="bg-gray-200 text-white">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div
+                class="flex flex-col md:flex-row justify-center md:justify-between items-center h-auto md:h-16 space-y-4 md:space-y-0">
+                <!-- Navbar -->
+                <div class="relative">
+                    <div class="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 w-full md:w-auto">
+                        <button
+                            class="w-full md:w-auto px-2 py-0 bg-emerald-600 text-white rounded-lg text-lg font-semibold shadow-md transition duration-300 ease-in-out hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50">
+                            Nuevo
+                        </button>
+                        <button
+                            class="w-full md:w-auto px-2 py-0 bg-emerald-600 text-white rounded-lg text-lg font-semibold shadow-md transition duration-300 ease-in-out hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50">
+                            Editar
+                        </button>
+                        <button
+                            class="w-full md:w-auto px-2 py-0 bg-emerald-600 text-white rounded-lg text-lg font-semibold shadow-md transition duration-300 ease-in-out hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50">
+                            Eliminar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
