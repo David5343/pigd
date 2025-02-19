9f71a76ec6c098c8d8cb98877a98a69fe1b83384
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-02-19 13:42:27 -0600

    limpiando archivo web de rutas para empezar a programar  acceso a afiliacion en la web

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\LegalDepartment\LegalDepartmentController;
 use App\Http\Controllers\MaterialResources\MaterialResourcesController;
 use App\Http\Controllers\MedicalCoordination\MedicalCoordinationController;
 use App\Http\Controllers\Roles\RoleController;
+use App\Http\Controllers\SocioeconomicBenefits\MembershipController;
 use App\Http\Controllers\SocioeconomicBenefits\SocioeconomicBenefitsController;
 use App\Http\Controllers\Users\UserController;
 use App\Http\Middleware\CheckIfActive;
@@ -20,15 +21,6 @@ Route::get('/', function () {
 
 Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',],[CheckIfActive::class])->group(function () {
     Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
-    Route::get('/general_coordination', [GeneralCoordinationController::class, 'index'])->name('gcoordination');
-    Route::get('/general_administration', [GeneralAdministrationController::class, 'index'])->name('gadministration.index');
-    Route::get('/legal_department', [LegalDepartmentController::class, 'index'])->name('ldepartment');
-    Route::get('/medical_coordination', [MedicalCoordinationController::class, 'index'])->name('mcoordination');
-    Route::get('/human_resources', [HumanResourcesController::class, 'index'])->name('hresources');
-    Route::get('/financial_resources', [FinancialResourcesController::class, 'index'])->name('fresources');
-    Route::get('/material_resources', [MaterialResourcesController::class, 'index'])->name('mresources');
-    Route::get('/socioeconomic_benefits', [SocioeconomicBenefitsController::class, 'index'])->name('scbenefits');
-    Route::get('/socioeconomic_benefits/membership', [SocioeconomicBenefitsController::class, 'membership'])->name('membership.index');
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-    Route::get('/roles', [RoleController::class, 'index'])->name('roles.index');
+    Route::get('/socioeconomic_benefits/membership', [MembershipController::class, 'index'])->name('membership.index');
+
 });
