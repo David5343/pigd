eb75fc6001c85f8df4114d84878e5c0770fe3b14
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-02-19 13:46:33 -0600

    creando  clases de livewire para el manejo de afiliacion, listado,editado,nuevo

---
+++ resources/views/livewire/socioeconomic-benefits/membership.blade.php
@@ -0,0 +1,47 @@
+<div>
+    <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+        <h1 class="mt-8 text-2xl font-medium text-gray-900">
+            Bienvenido a la Plataforma Institucional de Gestión de Datos
+        </h1>
+
+        <div class="flex flex-col">
+            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                    <div class="overflow-hidden">
+                        <table class="min-w-full text-left text-sm font-light text-surface dark:text-white">
+                            <thead class="border-b border-neutral-200 font-medium dark:border-white/10">
+                                <tr>
+                                    <th scope="col" class="px-6 py-4">#</th>
+                                    <th scope="col" class="px-6 py-4">First</th>
+                                    <th scope="col" class="px-6 py-4">Last</th>
+                                    <th scope="col" class="px-6 py-4">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="border-b border-neutral-200 dark:border-white/10">
+                                    <td class="whitespace-nowrap px-6 py-4 font-medium">1</td>
+                                    <td class="whitespace-nowrap px-6 py-4">Mark</td>
+                                    <td class="whitespace-nowrap px-6 py-4">Otto</td>
+                                    <td class="whitespace-nowrap px-6 py-4">@mdo</td>
+                                </tr>
+                                <tr class="border-b border-neutral-200 dark:border-white/10">
+                                    <td class="whitespace-nowrap px-6 py-4 font-medium">2</td>
+                                    <td class="whitespace-nowrap px-6 py-4">Jacob</td>
+                                    <td class="whitespace-nowrap px-6 py-4">Thornton</td>
+                                    <td class="whitespace-nowrap px-6 py-4">@fat</td>
+                                </tr>
+                                <tr class="border-b border-neutral-200 dark:border-white/10">
+                                    <td class="whitespace-nowrap px-6 py-4 font-medium">3</td>
+                                    <td class="whitespace-nowrap px-6 py-4">Larry</td>
+                                    <td class="whitespace-nowrap px-6 py-4">Wild</td>
+                                    <td class="whitespace-nowrap px-6 py-4">@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8"></div>
+</div>
