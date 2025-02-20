5a22bc4b24b3f59e03c4e5ac17f15191f094f7b5
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-02-20 17:48:56 -0600

    trabajando en modal en tailwind

--- resources/views/livewire/socioeconomic-benefits/membership.blade.php
+++ resources/views/livewire/socioeconomic-benefits/membership.blade.php
@@ -36,13 +36,75 @@
                                             </td>
                                             <td class="whitespace-nowrap px-6 py-4">{{ $item->rfc }}</td>
                                             <td class="whitespace-nowrap px-6 py-4">{{ $item->affiliate_status }}</td>
-                                            <td class="whitespace-nowrap px-6 py-4">{{ $item->id }}</td>
+                                            <td class="whitespace-nowrap px-6 py-4">
+                                                <button type="button" data-twe-toggle="modal"
+                                                    data-twe-target="#staticBackdrop" data-twe-ripple-init
+                                                    data-twe-ripple-color="light">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                        class="size-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
+                                                    </svg>
+                                                </button>
+                                            </td>
                                         </tr>
                                     @endforeach
                                 @endif
                             </tbody>
                         </table>
+                        <!-- Modal -->
+                        <div data-twe-modal-init
+                            class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+                            id="staticBackdrop" data-twe-backdrop="static" data-twe-keyboard="false" tabindex="-1"
+                            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div data-twe-modal-dialog-ref
+                                class="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
+                                <div
+                                    class="pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-4 outline-none dark:bg-surface-dark">
+                                    <div
+                                        class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 p-4 dark:border-white/10">
+                                        <!--Modal title-->
+                                        <h5 class="text-xl font-medium leading-normal text-surface dark:text-white"
+                                            id="exampleModalLabel">
+                                            Modal title
+                                        </h5>
+                                        <!-- Close button -->
+                                        <button type="button"
+                                            class="box-content rounded-none border-none text-neutral-500 hover:text-neutral-800 hover:no-underline focus:text-neutral-800 focus:opacity-100 focus:shadow-none focus:outline-none dark:text-neutral-400 dark:hover:text-neutral-300 dark:focus:text-neutral-300"
+                                            data-twe-modal-dismiss aria-label="Close">
+                                            <span class="[&>svg]:h-6 [&>svg]:w-6">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M6 18L18 6M6 6l12 12" />
+                                                </svg>
+                                            </span>
+                                        </button>
+                                    </div>
 
+                                    <!-- Modal body -->
+                                    <div class="relative flex justify-center p-4" data-twe-modal-body-ref>
+                                        ...
+                                    </div>
+
+                                    <!-- Modal footer -->
+                                    <div
+                                        class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 p-4 dark:border-white/10">
+                                        <button type="button"
+                                            class="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary-accent-200 focus:bg-primary-accent-200 focus:outline-none focus:ring-0 active:bg-primary-accent-200 dark:bg-primary-300 dark:hover:bg-primary-400 dark:focus:bg-primary-400 dark:active:bg-primary-400"
+                                            data-twe-modal-dismiss data-twe-ripple-init data-twe-ripple-color="light">
+                                            Close
+                                        </button>
+                                        <button type="button"
+                                            class="ms-1 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+                                            data-twe-ripple-init data-twe-ripple-color="light">
+                                            Understood
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 {{ $lista->links() }}
