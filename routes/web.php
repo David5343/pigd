744a322792e23b4eff95af4a2cec95899b2f38bd
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-02-21 21:09:20 -0600

    agregando  ruta para mostrar datos del registro de afiliados

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',],[CheckIfActive::class])->group(function () {
     Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
     Route::get('/socioeconomic_benefits/membership', [MembershipController::class, 'index'])->name('membership.index');
+    Route::get('/socioeconomic_benefits/membership/{id}', [MembershipController::class, 'show'])->name('membership.show');
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
 });
