6669342ad762881e50fb57d6efd37dc17cd90e8f
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-02-21 21:09:56 -0600

    agregando metodo show al controlador de afiliados

--- app/Http/Controllers/SocioeconomicBenefits/MembershipController.php
+++ app/Http/Controllers/SocioeconomicBenefits/MembershipController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\SocioeconomicBenefits;
 
 use App\Http\Controllers\Controller;
+use App\Models\SocioeconomicBenefits\Insured;
 use Illuminate\Http\Request;
 
 class MembershipController extends Controller
@@ -10,6 +11,12 @@ class MembershipController extends Controller
     public function index()
     {
         return view('socioeconomic_benefits.membership.index');
+
+    }
+    public function show(string $id)
+    {
+        $row = Insured::find($id);
+        return view('socioeconomic_benefits.membership.show',['titular'=> $row]);
 
     }
 }
