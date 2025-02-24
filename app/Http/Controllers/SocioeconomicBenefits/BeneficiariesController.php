2776611143b2510bd21e2b8eafcc0c9172d29830
David <60202438+David5343@users.noreply.github.com>
2025-02-23 20:47:54 -0600

    creando controlador de familiares con metodo index y show

---
+++ app/Http/Controllers/SocioeconomicBenefits/BeneficiariesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\SocioeconomicBenefits;
+
+use App\Http\Controllers\Controller;
+use App\Models\SocioeconomicBenefits\Beneficiary;
+use Illuminate\Http\Request;
+
+class BeneficiariesController extends Controller
+{
+    public function index()
+    {
+        return view('socioeconomic_benefits.beneficiaries.index');
+
+    }
+    public function show(string $id)
+    {
+        $row = Beneficiary::where('id', $id)
+            ->with('insured')
+            ->first();
+        return view('socioeconomic_benefits.beneficiaries.show',['familiar'=> $row]);
+
+    }
+}
