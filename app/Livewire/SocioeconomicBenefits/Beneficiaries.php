0555767acac5f79140caad0f35af9d94d4d5ca43
David <60202438+David5343@users.noreply.github.com>
2025-02-23 20:48:53 -0600

    agreganco componente livewire de familiares

---
+++ app/Livewire/SocioeconomicBenefits/Beneficiaries.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire\SocioeconomicBenefits;
+
+use App\Models\SocioeconomicBenefits\Beneficiary;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Beneficiaries extends Component
+{
+    use WithPagination;
+    public $search = '';
+    public $numberRows = 10;
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+    public function updatingnumberRows()
+    {
+        $this->resetPage();
+    }
+    public function render()
+    {
+        $lista = Beneficiary::where('status', 'active')
+        ->where(function($query) {
+            $query->where('file_number', 'like', '%'.$this->search.'%')
+                  ->orWhere('name', 'like', '%'.$this->search.'%')
+                  ->orWhere('last_name_1', 'like', '%'.$this->search.'%')
+                  ->orWhere('last_name_2', 'like', '%'.$this->search.'%')
+                  ->orWhere('curp', 'like', '%'.$this->search.'%');
+        })
+        ->latest() // Equivalente a orderBy('created_at', 'desc')
+        ->take(50) // Tomar solo los últimos 50 registros
+        ->paginate($this->numberRows);
+        return view('livewire.socioeconomic-benefits.beneficiaries',['lista'=>$lista]);
+    }
+}
