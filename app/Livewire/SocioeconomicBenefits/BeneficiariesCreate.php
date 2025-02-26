a340e7d0302cb620cb17d44ff1bd71d4b7ddee09
David <60202438+David5343@users.noreply.github.com>
2025-02-25 21:10:06 -0600

    trabajando en componente de familiar para la captura

--- app/Livewire/SocioeconomicBenefits/BeneficiariesCreate.php
+++ app/Livewire/SocioeconomicBenefits/BeneficiariesCreate.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire\SocioeconomicBenefits;
 
+use Haruncpi\LaravelIdGenerator\IdGenerator;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class BeneficiariesCreate extends Component
 {
+    #[Validate('required|max:8|unique:beneficiaries,file_number')]
+    public $folio;
     public function render()
     {
-        return view('livewire.socioeconomic-benefits.beneficiaries-create');
+        $this->folio = IdGenerator::generate(['table' => 'beneficiaries', 'field' => 'file_number', 'length' => 8, 'prefix' => 'F']);
+        return view('livewire.socioeconomic-benefits.beneficiaries-create',['folio' => $this->folio]);
     }
 }
