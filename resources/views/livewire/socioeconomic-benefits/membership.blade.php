15d35e703b6d38f739f91bdbebfe52404f0d0f3a
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-02-27 15:22:25 -0600

    agregando boton editar en listado y busqueda de titular

--- resources/views/livewire/socioeconomic-benefits/membership.blade.php
+++ resources/views/livewire/socioeconomic-benefits/membership.blade.php
@@ -32,6 +32,7 @@
                                     <th scope="col" class="px-6 py-4">Nombre</th>
                                     <th scope="col" class="px-6 py-4">RFC</th>
                                     <th scope="col" class="px-6 py-4">Estatus</th>
+                                    <th scope="col" class="px-6 py-4">Editar</th>
                                     <th scope="col" class="px-6 py-4">Ver</th>
                                 </tr>
                             </thead>
@@ -47,6 +48,19 @@
                                             <td class="whitespace-nowrap px-6 py-4">{{ $item->rfc }}</td>
                                             <td class="whitespace-nowrap px-6 py-4">{{ $item->affiliate_status }}</td>
                                             <td class="whitespace-nowrap px-6 py-4">
+                                                <a
+                                                    href="{{ url('socioeconomic_benefits/membership/' . $item->id . '/edit') }}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                        class="size-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                    </svg>
+
+                                                </a>
+                                            </td>
+                                            <td class="whitespace-nowrap px-6 py-4">
+
                                                 <a href="{{ url('socioeconomic_benefits/membership/' . $item->id) }}">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none"
                                                         viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
