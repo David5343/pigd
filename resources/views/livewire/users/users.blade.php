bc2f490f3ae97a59bb58012dd3bd249c09a55ba3
David <60202438+David5343@users.noreply.github.com>
2025-03-12 23:08:41 -0600

    agregando estilos y elementos de diseño en listar usuarios

--- resources/views/livewire/users/users.blade.php
+++ resources/views/livewire/users/users.blade.php
@@ -32,31 +32,75 @@
                                     <th scope="col" class="px-6 py-4">Usuario</th>
                                     <th scope="col" class="px-6 py-4">Rol</th>
                                     <th scope="col" class="px-6 py-4">Api Token</th>
+                                    <th scope="col" class="px-6 py-4">Estatus</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @if ($users->count())
                                     @foreach ($users as $item)
                                         <tr class="border-b border-neutral-200 dark:border-white/10">
-                                            <td class="whitespace-nowrap px-6 py-4 font-medium">{{ $item->id }}</td>
-                                            <td class="whitespace-nowrap px-6 py-4">{{ $item->name }}</td>
-                                            <td class="whitespace-nowrap px-6 py-4">{{ $item->email }}</td>
-                                            <td class="whitespace-nowrap px-6 py-4">
-                                                {{ $item->roles->pluck('name')->implode(', ') }}
+                                            <td class="w-16 px-6 py-4 font-medium"">{{ $item->id }}</td>
+                                            <td class="w-40 px-6 py-4">{{ $item->name }}</td>
+                                            <td class="w-60 px-6 py-4">{{ $item->email }}</td>
+                                            <td class="w-40 px-6 py-4">
+                                                @if ($item->roles->isNotEmpty())
+                                                    <span
+                                                        class="px-2 py-1 bg-blue-100 text-blue-700 rounded">{{ $item->roles->pluck('name')->implode(', ') }}</span>
+                                                @else
+                                                    <span class="px-2 py-1 bg-yellow-100 text-yellow-700 rounded">Sin
+                                                        rol</span>
+                                                @endif
                                             </td>
-                                            <td class="whitespace-nowrap px-6 py-4">
-                                                <button type="button" data-twe-toggle="modal"
-                                                    data-twe-target="#staticBackdrop" data-twe-ripple-init
-                                                    data-twe-ripple-color="light">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                        class="size-6">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
-                                                    </svg>
-
-                                                </button>
+                                            {{-- <td class="w-16 px-6 py-4">
+                                                @if ($item->api_token === null)
+                                                    <button type="button" data-twe-toggle="modal"
+                                                        data-twe-target="#staticBackdrop" data-twe-ripple-init
+                                                        data-twe-ripple-color="light">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                            class="size-6">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
+                                                        </svg>
+                                                    </button>
+                                                @else
+                                                    {{ $item->api_token }}
+                                                @endif
+                                            </td> --}}
+                                            <td class="w-16 px-6 py-4 relative group">
+                                                @if ($item->api_token === null)
+                                                    <button type="button" data-twe-toggle="modal"
+                                                        data-twe-target="#staticBackdrop" data-twe-ripple-init
+                                                        data-twe-ripple-color="light">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                            class="size-6">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                        </svg>
+                                                    </button>
+                                                @else
+                                                    <div class="relative">
+                                                        <button
+                                                            class="bg-gray-200 p-1 rounded hover:bg-gray-300 transition group">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                viewBox="0 0 24 24" stroke-width="1.5"
+                                                                stroke="currentColor" class="size-6">
+                                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                                    d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
+                                                            </svg>
+                                                        </button>
+                                                        <!-- Tooltip -->
+                                                        <div
+                                                            class="absolute top-8 left-1/2 -translate-x-1/2 bg-black text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition">
+                                                            {{ $item->api_token }}
+                                                        </div>
+                                                    </div>
+                                                @endif
                                             </td>
+                                            <td class="w-40 px-6 py-4">{{ $item->id }}</td>
                                         </tr>
                                     @endforeach
                                 @endif
