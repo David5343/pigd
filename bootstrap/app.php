23f69d799caf632ef0221ee2eecc2b9b0935403b
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-03-18 18:22:11 -0600

    trabajando en middleware de superadmin y registrando el control de roles y permisos

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Middleware\CheckIfActive;
+use App\Http\Middleware\SuperAdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,7 +16,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(CheckIfActive::class);
+            // Agregar middlewares aquí
+            $middleware->append(CheckIfActive::class);
+            //$middleware->append(SuperAdminMiddleware::class);
+            $middleware->alias([
+                'role' => RoleMiddleware::class,
+                'permission' => PermissionMiddleware::class,
+            ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
