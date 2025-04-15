a40638e12c0159e282fd8209165dc210441aeaf2
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-04-15 11:13:40 -0600

    agregando clase para usar el sofdeletes en clase positions

--- app/Models/Catalogs/Position.php
+++ app/Models/Catalogs/Position.php
@@ -4,9 +4,12 @@ namespace App\Models\Catalogs;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Position extends Model
 {
+    use SoftDeletes;
+    
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
