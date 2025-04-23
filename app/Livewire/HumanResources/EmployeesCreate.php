dff857167d672aa2249783abb5251d196e1d4662
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-04-22 21:45:58 -0600

    trabajando en componente de clase para guardar empleado, terminado validaciones y listado de todas las variables a recolectar

--- app/Livewire/HumanResources/EmployeesCreate.php
+++ app/Livewire/HumanResources/EmployeesCreate.php
@@ -14,19 +14,82 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class EmployeesCreate extends Component
 {
+    use WithFileUploads;
+
     public $areas = [];
     public $positions = [];
     public $banks= [];
     public $states;
-    public $state_id = '';
     public $counties = [];
-    public $county_id = '';
     protected $listeners = ['refreshComponent' => '$refresh']; // Escucha el evento refreshComponent
-
-
+    #[Validate('required')]
+    public $mov_type = '';
+    #[Validate('required')]
+    public $contract_type = '';
+    #[Validate('required|unique:employees,position_id')]
+    public $position_id = '';
+    #[Validate('required')]
+    public $area_id = '';
+    #[Validate('required|date')]
+    public $start_date = '';
+    #[Validate('required|min:2|max:20')]
+    public $last_name_1;
+    #[Validate('nullable|min:2|max:20')]
+    public $last_name_2;
+    #[Validate('required|min:2|max:30')]
+    public $name;
+    #[Validate('required|date')]
+    public $birthday;
+    #[Validate('required')]
+    public $sex;
+    #[Validate('required')]
+    public $marital_status;
+    #[Validate('required|min:10|max:13|alpha_num:ascii')]
+    public $rfc;
+    #[Validate('required|min:10|max:18|alpha_num:ascii')]
+    public $curp;
+    #[Validate('required|numeric|digits:10')]
+    public $phone;
+    #[Validate('required|email|min:5|max:50|unique:employees,email')]
+    public $email;
+    #[Validate('required|min:3|max:30')]
+    public $emergency_name;
+    #[Validate('required|numeric|digits:10')]
+    public $emergency_number;
+    #[Validate('required|min:3|max:30')]
+    public $emergency_address;
+    #[Validate('required')]
+    public $state_id = '';
+    #[Validate('required')]
+    public $county_id = '';
+    #[Validate('required|min:5|max:50')]
+    public $neighborhood;
+    #[Validate('required|min:5|max:50')]
+    public $roadway_type;
+    #[Validate('required|min:5|max:50')]
+    public $street;
+    #[Validate('required|max:7')]
+    public $outdoor_number;
+    #[Validate('required|max:7')]
+    public $interior_number;
+    #[Validate('required|numeric|digits:5')]
+    public $cp;
+    #[Validate('required|min:5|max:85')]
+    public $locality;
+    #[Validate('required|numeric|digits:10')]
+    public $account_number;
+    #[Validate('required|numeric|digits:18')]
+    public $clabe;
+    #[Validate('required')]
+    public $bank_id;
+    #[Validate('required|image|mimes:jpeg,jpg|max:1024')]
+    public $photo;
+    #[Validate('required|image|mimes:jpeg,jpg|max:1024')]
+    public $signature;
 
     public function mount()
     {
