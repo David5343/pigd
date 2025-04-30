b832ab4800fabba41c4a6c501d9af5b91cba7c48
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-04-30 11:33:56 -0600

    componente para listar empleado y buscar empleados

--- app/Models/HumanResources/Employee.php
+++ app/Models/HumanResources/Employee.php
@@ -2,9 +2,19 @@
 
 namespace App\Models\HumanResources;
 
+use App\Models\Catalogs\Area;
+use App\Models\Catalogs\Position;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Employee extends Model
 {
-    //
+    public function position(): BelongsTo
+    {
+        return $this->belongsTo(Position::class);
+    }
+    public function area(): BelongsTo
+    {
+        return $this->belongsTo(Area::class);
+    }
 }
