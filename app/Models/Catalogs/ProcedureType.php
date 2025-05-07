6c4532a73ef269c73065f2fa84ee5dfe269ce0c8
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-05-07 15:55:47 -0600

    creando clase modelo de tipo de movimientos nominales agregando softdeletes

--- app/Models/Catalogs/ProcedureType.php
+++ app/Models/Catalogs/ProcedureType.php
@@ -3,8 +3,9 @@
 namespace App\Models\Catalogs;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProcedureType extends Model
 {
-    //
+    use SoftDeletes;
 }
