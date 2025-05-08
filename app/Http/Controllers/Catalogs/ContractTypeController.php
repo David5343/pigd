647ad10237e4ad7a049bc8a8a8f90b8d4cb7ebc5
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-05-08 10:07:08 -0600

    creando controlador para tipo de contrato laboral con sus metodo index,create,edit,update funcional

---
+++ app/Http/Controllers/Catalogs/ContractTypeController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Catalogs;
+
+use App\Http\Controllers\Controller;
+use App\Livewire\Catalogs\ContractType as CatalogsContractType;
+use App\Models\Catalogs\ContractType;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class ContractTypeController extends Controller
+{
+    public function index()
+    {
+        return view('catalogs.contract-type.index');
+
+    }
+    public function create()
+    {
+        return view('catalogs.contract-type.create');
+
+    }
+    public function edit(string $id)
+    {
+        $row = ContractType::find($id);
+        return view('catalogs.contract-type.edit', ['row' => $row]);
+    }
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'name' => ['required', 'min:2', 'max:30', 'unique:procedure_types,name,'.$id],
+            'description' => ['nullable','min:2', 'max:200'],
+
+        ]);
+
+        try {
+            DB::beginTransaction();
+            $contract = CatalogsContractType::find($id);
+            $contract->name = Str::of($request->input('name'))->trim();
+            $contract->description = Str::of($request->input('description'))->trim();
+            $contract->modified_by = Auth::user()->email;
+            $contract->save();
+            DB::commit();
+            return back()->with('msg', 'Tipo de contrato actualizado.');
+        } catch (Exception $e) {
+            DB::rollBack();
+            return back()->with('msg_warning', 'Error: ' . $e->getMessage());
+        }
+    }
+}
