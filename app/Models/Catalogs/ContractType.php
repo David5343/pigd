dc8faad00795751982b60cf62e6c3263bd1cb157
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-05-08 10:08:08 -0600

    creando clase modelo de tipo de contrato laboral y agregando soft delete al modelo

--- app/Models/Catalogs/ContractType.php
+++ app/Models/Catalogs/ContractType.php
@@ -3,8 +3,9 @@
 namespace App\Models\Catalogs;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ContractType extends Model
 {
-    //
+    use SoftDeletes;
 }
