141fc8bf174cf785fc01c46b43b08f31b8be9913
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-05-13 15:02:33 -0600

    agregando a modelo de empleado la relacion con tipo de movimiento, area, puesto, y dando formato a la columna start_date para tipo fecha format('d/m/Y')

--- app/Models/HumanResources/Employee.php
+++ app/Models/HumanResources/Employee.php
@@ -4,12 +4,24 @@ namespace App\Models\HumanResources;
 
 use App\Models\Catalogs\Area;
 use App\Models\Catalogs\Position;
+use App\Models\Catalogs\ProcedureType;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employee extends Model
 {
+    protected $casts = [
+        'start_date' => 'date', // Asegura que sea Carbon en lugar de string
+    ];
+
+    protected function startDateFormatted(): Attribute
+    {
+        return Attribute::get(
+            fn () => $this->start_date?->format('d/m/Y')
+        );
+    }
     public function position(): BelongsTo
     {
         return $this->belongsTo(Position::class);
@@ -18,8 +30,8 @@ class Employee extends Model
     {
         return $this->belongsTo(Area::class);
     }
-    public function procedures():HasMany
-{
-    return $this->hasMany(EmployeeProcedure::class);
-}
+    public function procedureType():BelongsTo
+    {
+        return$this->belongsTo(ProcedureType::class);
+    }
 }
