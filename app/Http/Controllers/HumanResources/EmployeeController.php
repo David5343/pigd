5dc226f75b724a2e9cbd0be49b7ccc4e4de89411
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-05-13 15:24:05 -0600

    agregando metodo show en controler de empleado

--- app/Http/Controllers/HumanResources/EmployeeController.php
+++ app/Http/Controllers/HumanResources/EmployeeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\HumanResources;
 
 use App\Http\Controllers\Controller;
+use App\Models\HumanResources\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
@@ -16,5 +17,9 @@ class EmployeeController extends Controller
     {
         return view('human_resources.employees.create');
 
+    }
+    public function show(string $id)
+    {
+        $employe = Employee::fine($id);
     }
 }
