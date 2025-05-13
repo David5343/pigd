21d2ad1535238f55699cccf04369727dd542870f
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-05-13 15:00:53 -0600

    agregando relaciones  a la tabla movimientos nomimales con empleados, tip de movimiento,tipo de contrato, area,puesto y dando formato a columnas como created_at y effective_date

--- app/Models/HumanResources/EmployeeProcedure.php
+++ app/Models/HumanResources/EmployeeProcedure.php
@@ -2,13 +2,52 @@
 
 namespace App\Models\HumanResources;
 
+use App\Models\Catalogs\Area;
+use App\Models\Catalogs\ContractType;
+use App\Models\Catalogs\Position;
+use App\Models\Catalogs\ProcedureType;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EmployeeProcedure extends Model
 {
+
+    protected $casts = [
+        'effective_date' => 'date',
+        'created_at' => 'date', // Asegura que sea Carbon en lugar de string
+    ];
+
+    protected function createdAtFormatted(): Attribute
+    {
+        return Attribute::get(
+            fn () => $this->created_at?->format('d/m/Y')
+        );
+    }
+    protected function effectiveDateFormatted(): Attribute
+    {
+        return Attribute::get(
+            fn () => $this->effective_date?->format('d/m/Y')
+        );
+    }
     public function employee():BelongsTo
     {
         return $this->belongsTo(Employee::class);
+    }
+    public function procedureType():BelongsTo
+    {
+        return$this->belongsTo(ProcedureType::class);
+    }
+    public function contractType():BelongsTo
+    {
+        return$this->belongsTo(ContractType::class);
+    }
+    public function area(): BelongsTo
+    {
+        return $this->belongsTo(Area::class);
+    }
+    public function position(): BelongsTo
+    {
+        return $this->belongsTo(Position::class);
     }
 }
