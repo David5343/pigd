3c1205cf77cafe03b97e41dcc5e9b16e428388f2
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-05-13 15:13:27 -0600

    agregando metodo index y create en controlador movimiento nominal para contener los componentes livewire

--- app/Http/Controllers/HumanResources/EmployeeProcedureController.php
+++ app/Http/Controllers/HumanResources/EmployeeProcedureController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 
 class EmployeeProcedureController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('human_resources.employee-procedure.index');
+
+    }
+    public function create()
+    {
+        return view('human_resources.employee-procedure.create');
+
+    }
 }
