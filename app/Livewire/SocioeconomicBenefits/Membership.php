581e9580dc8dda2020bd21cea2725697842af7fe
Trabajo <124404479+Sacd2023@users.noreply.github.com>
2025-05-15 14:52:30 -0600

    estadisticas rapidas de titulares

--- app/Livewire/SocioeconomicBenefits/Membership.php
+++ app/Livewire/SocioeconomicBenefits/Membership.php
@@ -19,10 +19,26 @@ class Membership extends Component
     {
         $this->resetPage();
     }
+    public function getInsuredPreProperty()
+    {
+        return Insured::where('affiliate_status','Preafiliado')->count();
+    }
+    public function getInsuredActivosProperty()
+    {
+        return Insured::where('affiliate_status','Activo')->count();
+    }
+    public function getInsuredBajasProperty()
+    {
+        return Insured::where('affiliate_status','Baja')->count();
+    }
+    public function getInsuredBajasPendientesProperty()
+    {
+        return Insured::where('affiliate_status','Baja por Aplicar')->count();
+    }
     public function render()
     {
-        $lista = Insured::where('status', 'active')
-        ->where(function($query) {
+        // $lista = Insured::where('status', 'active')
+        $lista = Insured::where(function($query) {
             $query->where('file_number', 'like', '%'.$this->search.'%')
                   ->orWhere('name', 'like', '%'.$this->search.'%')
                   ->orWhere('last_name_1', 'like', '%'.$this->search.'%')
