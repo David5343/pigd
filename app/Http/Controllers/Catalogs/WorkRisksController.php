d74098160c2fbe22d8010bd9e00fda780d3f9199
Trabajo <david_5343@hotmail.com>
2025-06-19 23:16:40 -0600

    agregando controlador de tipo de riesgotrabajo con funcionalidad

---
+++ app/Http/Controllers/Catalogs/WorkRisksController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Catalogs;
+
+use App\Http\Controllers\Controller;
+use App\Models\Catalogs\PensionType;
+use App\Models\Catalogs\WorkRisks;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class WorkRisksController extends Controller
+{
+    public function index()
+    {
+        return view('catalogs.work-risk.index');
+
+    }
+    public function create()
+    {
+        return view('catalogs.work-risk.create');
+
+    }
+    public function edit(string $id)
+    {
+        $row = WorkRisks::find($id);
+        $pension_types = PensionType::all();
+        return view('catalogs.work-risk.edit', ['row' => $row,'pensiones'=>$pension_types]);
+    }
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'name' => ['required','min:5', 'max:70'],
+            'pension_type_id'=> ['required']
+        ]);
+
+        try {
+            DB::beginTransaction();
+            $work_risks = WorkRisks::find($id);
+
+            if (!$work_risks) {
+                return back()->with('msg_warning', 'Tipo de riesgo no encontrado');
+            }
+
+            $work_risks->name = Str::of($request->input('name'))->trim();
+            $work_risks->pension_type_id =$request->input('dependency_id');
+            $work_risks->modified_by = Auth::user()->email;
+            $work_risks->save();
+            DB::commit();
+            return back()->with('msg', 'Tipo de riesgo actualizado.');
+        } catch (Exception $e) {
+            DB::rollBack();
+            return back()->with('msg_warning', 'Error: ' . $e->getMessage());
+        }
+    }
+}
