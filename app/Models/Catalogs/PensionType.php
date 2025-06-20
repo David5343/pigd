c289fc3d941c68d51053d2a89b3dde309fd2ad3c
Trabajo <david_5343@hotmail.com>
2025-06-19 23:10:37 -0600

    agregando relacion entre tipo de pension y tipo de riesgo de trabajo

--- app/Models/Catalogs/PensionType.php
+++ app/Models/Catalogs/PensionType.php
@@ -3,8 +3,12 @@
 namespace App\Models\Catalogs;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PensionType extends Model
 {
-    //
+    public function workRisks(): HasMany
+    {
+        return $this->hasMany(WorkRisks::class);
+    }
 }
