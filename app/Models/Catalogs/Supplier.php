9998cee8cceb3f4ffdf2fdb781fd2750aa91df4a
Trabajo <david_5343@hotmail.com>
2025-07-12 17:08:51 -0600

    agregando modelo de proveedores

---
+++ app/Models/Catalogs/Supplier.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Catalogs;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+    protected $fillable = ['name', 'rfc', 'type'];
+
+        public function categories()
+    {
+        return $this->belongsToMany(SupplierCategory::class, 'supplier_supplier_category');
+    }
+}
