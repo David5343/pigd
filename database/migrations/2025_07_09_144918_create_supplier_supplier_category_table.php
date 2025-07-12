5a8abd29660e13a7f4733721a0129ad1e6242ea8
Trabajo <david_5343@hotmail.com>
2025-07-12 16:58:50 -0600

    creando migracion para tabla pivote  proveedores y categoria de proveedores

---
+++ database/migrations/2025_07_09_144918_create_supplier_supplier_category_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supplier_supplier_category', function (Blueprint $table) {
+            $table->foreignId('supplier_id')->constrained()->onDelete('cascade');
+            $table->foreignId('supplier_category_id')->constrained()->onDelete('cascade');
+            $table->primary(['supplier_id', 'supplier_category_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supplier_supplier_category');
+    }
+};
