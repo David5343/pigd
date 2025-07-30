4ea5aec3c4bc8c154472fd20b6d2d4a0940c1a46
Trabajo <david_5343@hotmail.com>
2025-07-30 09:52:04 -0600

    agregando relacion de medicamentos en el modelo de proveedores

--- app/Models/Catalogs/Supplier.php
+++ app/Models/Catalogs/Supplier.php
@@ -8,8 +8,12 @@ class Supplier extends Model
 {
     protected $fillable = ['name', 'rfc', 'type'];
 
-        public function categories()
+    public function categories()
     {
         return $this->belongsToMany(SupplierCategory::class, 'supplier_supplier_category');
+    }
+    public function medications()
+    {
+        return $this->hasMany(Medication::class);
     }
 }
