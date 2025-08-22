8a424c946eb768951b1caa9893bf1f9e3537d2b0
Trabajo <david_5343@hotmail.com>
2025-08-22 13:55:33 -0600

    ajustes en las relaciones entre tablas de insured con otras tablas

--- app/Models/SocioeconomicBenefits/Insured.php
+++ app/Models/SocioeconomicBenefits/Insured.php
@@ -2,7 +2,9 @@
 
 namespace App\Models\SocioeconomicBenefits;
 
+use App\Models\Catalogs\AffiliationStatus;
 use App\Models\Catalogs\Bank;
+use App\Models\Catalogs\County;
 use App\Models\Catalogs\Subdependency;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,31 +14,36 @@ class Insured extends Model
 {
     protected $table = 'insureds';
 
-    public function beneficiaries(): HasMany
-    {
-        return $this->hasMany(Beneficiary::class);
-    }
 
     public function subdependency(): BelongsTo
     {
         return $this->belongsTo(Subdependency::class);
     }
 
-    public function bank(): BelongsTo
-    {
-        return $this->belongsTo(Bank::class);
-    }
-
     public function rank(): BelongsTo
     {
         return $this->belongsTo(Rank::class);
     }
-
+    public function workplaceCounty()
+    {
+        return $this->belongsTo(County::class, 'workplace_county_id');
+    }
+        public function birthplaceCounty()
+    {
+        return $this->belongsTo(County::class, 'birthplace_county_id');
+    }
+        public function county()
+    {
+        return $this->belongsTo(County::class, 'county_id');
+    }
+        public function beneficiaries(): HasMany
+    {
+        return $this->hasMany(Beneficiary::class);
+    }
     public function credencialInsured(): HasMany
     {
         return $this->hasMany(CredentialInsured::class);
     }
-
     public function tickets()
     {
         return $this->hasMany(Ticket::class);
@@ -45,5 +52,9 @@ class Insured extends Model
     public function retiree(): HasMany
     {
         return $this->hasMany(Retiree::class);
+    }
+    public function affiliationStatus()
+    {
+        return $this->belongsTo(AffiliationStatus::class);
     }
 }
