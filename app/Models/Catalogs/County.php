79cff46ea74e1dbc65fe014277721c81ef00dd33
Trabajo <david_5343@hotmail.com>
2025-08-22 13:56:18 -0600

    agregando relaciones con  insured en distintos campos

--- app/Models/Catalogs/County.php
+++ app/Models/Catalogs/County.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Catalogs;
 
+use App\Models\SocioeconomicBenefits\Insured;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -10,5 +11,17 @@ class County extends Model
     public function state(): BelongsTo
     {
         return $this->belongsTo(State::class);
+    }
+    public function workplaceInsureds()
+    {
+        return $this->hasMany(Insured::class, 'workplace_county_id');
+    }
+    public function birthplaceInsureds()
+    {
+        return $this->hasMany(Insured::class, 'birthplace_county_id');
+    }
+    public function insureds()
+    {
+        return $this->hasMany(Insured::class, 'county_id');
     }
 }
