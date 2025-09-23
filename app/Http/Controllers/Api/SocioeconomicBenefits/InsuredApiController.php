2811008de8b65356a063d8fba49679ec719e7858
Trabajo <david_5343@hotmail.com>
2025-09-23 13:20:13 -0600

    agregando metodo de reingreso y ajustes para el uso del estatus de afiliacion como catalogo

--- app/Http/Controllers/Api/SocioeconomicBenefits/InsuredApiController.php
+++ app/Http/Controllers/Api/SocioeconomicBenefits/InsuredApiController.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class InsuredApiController extends Controller
 {
@@ -147,7 +148,7 @@ class InsuredApiController extends Controller
             'File_number' => [
                 'required',
                 'max:8',
-                Rule::unique('insureds')->where(fn(Builder $query) => $query->where('affiliate_status', 'Activo')),
+                Rule::unique('insureds')->where(fn(Builder $query) => $query->where('affiliation_status_id', 2)),
             ],
             'Employee_number' => 'required|min:6|max:6|unique:insureds,employee_number',
             'Start_date' => 'required|date|max:10',
@@ -162,7 +163,7 @@ class InsuredApiController extends Controller
             'Rfc' => [
                 'required',
                 'alpha_num:ascii',
-                Rule::unique('insureds')->where(fn(Builder $query) => $query->where('affiliate_status', 'Activo')),
+                Rule::unique('insureds')->where(fn(Builder $query) => $query->where('affiliation_status_id', 2)),
             ],
             'Curp' => 'nullable | string | min:18 | max: 18',
             'Phone' => 'nullable|numeric|digits:10',
@@ -389,12 +390,13 @@ class InsuredApiController extends Controller
 
             $insured = Insured::with($relations)
                 ->where('file_number', $folio)
+                ->where('affiliation_status_id', '!=', 4)
                 ->first();
 
             $history = Insured::with($relations)
                 ->where('file_number', $folio)
-                ->where('affiliate_status', 'Baja')
-                ->first();
+                ->where('affiliation_status_id', 4)
+                ->get();
 
             if (!$insured && !$history) {
                 return response()->json([
@@ -433,13 +435,14 @@ class InsuredApiController extends Controller
             ];
 
             $insured = Insured::with($relations)
-                ->where('rfc', $rfc)
+                ->where('file_number', $rfc)
+                ->where('affiliation_status_id', '!=', 4)
                 ->first();
 
             $history = Insured::with($relations)
-                ->where('rfc', $rfc)
-                ->where('affiliate_status', 'Baja')
-                ->first();
+                ->where('file_number', $rfc)
+                ->where('affiliation_status_id', 4)
+                ->get();
 
             if (!$insured && !$history) {
                 return response()->json([
@@ -464,7 +467,6 @@ class InsuredApiController extends Controller
             ], 500);
         }
     }
-
     public function searchByCurp(Request $request, $curp)
     {
         try {
@@ -477,15 +479,16 @@ class InsuredApiController extends Controller
                 'affiliationStatus',
                 'beneficiaries'
             ];
-
+            
             $insured = Insured::with($relations)
-                ->where('curp', $curp)
+                ->where('file_number', $curp)
+                ->where('affiliation_status_id', '!=', 4)
                 ->first();
 
             $history = Insured::with($relations)
-                ->where('curp', $curp)
-                ->where('affiliate_status', 'Baja')
-                ->first();
+                ->where('file_number', $curp)
+                ->where('affiliation_status_id', 4)
+                ->get();
 
             if (!$insured && !$history) {
                 return response()->json([
@@ -509,7 +512,7 @@ class InsuredApiController extends Controller
                 'error' => $e->getMessage(),
             ], 500);
         }
-    }
+    }    
     public function deactivate(Request $request, $id)
     {
         $rules = [
@@ -710,6 +713,114 @@ class InsuredApiController extends Controller
             ], 200);
         } catch (\Exception $e) {
             DB::rollBack();
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Error en el servidor',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
+    }
+    public function reentry(Request $request)
+    {
+
+        $rules = [
+            'Subdependency_id' => 'required',
+            'Rank_id' => 'required',
+            'Workplace_county_id' => 'nullable',
+            'Birthplace_county_id' => 'nullable',
+            'Affiliation_status_id' => 'required',
+            'County_id' => 'nullable',
+            //Valida si ya hay un registro en la tabla insureds con el mismo numero de afiliacion con un estatus de activo
+            'File_number' => [
+                'required',
+                'max:8',
+                Rule::unique('insureds')->where(fn(Builder $query) => $query->where('affiliation_status_id', 2)),
+            ],
+            'Employee_number' => 'required|min:6|max:6|unique:insureds,employee_number',
+            'Start_date' => 'required|date|max:10',
+            'Register_motive' => 'nullable|min:3|max:250',
+            'Observations' => 'nullable|min:5|max:250',
+            'Last_name_1' => 'required|min:2|max:20',
+            'Last_name_2' => 'nullable|min:2|max:20',
+            'Name' => 'required|min:2|max:30',
+            'Birthday' => 'nullable|max:10|date',
+            'Sex' => 'required',
+            'Marital_status' => 'nullable',
+            'Rfc' => [
+                'required',
+                'alpha_num:ascii',
+                Rule::unique('insureds')->where(fn(Builder $query) => $query->where('affiliation_status_id', 2)),
+            ],
+            'Curp' => 'nullable | string | min:18 | max: 18',
+            'Phone' => 'nullable|numeric|digits:10',
+            'Email' => 'nullable|email|min:5|max:50|unique:insureds,email',
+            'Neighborhood' => 'nullable|min:5|max:50',
+            'Roadway_type' => 'nullable|min:5|max:50',
+            'Street' => 'nullable|min:5|max:50',
+            'Outdoor_number' => 'nullable|max:7',
+            'Interior_number' => 'nullable|max:7',
+            'Cp' => 'nullable|numeric|digits:5',
+            'locality' => 'nullable|min:5|max:85',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        // Comprobar si la validación falla
+        if ($validator->fails()) {
+            // Retornar errores de validación
+            return response()->json([
+                'status' => 'warning',
+                'message' => 'Error de validación',
+                'insured' => null,
+                'errors' => $validator->errors()->toArray(),
+            ], 422);
+        }
+        DB::beginTransaction();
+        try {
+            $insured = new Insured();
+            $insured->subdependency_id = $request->input('Subdependency_id');
+            $insured->rank_id = $request->input('Rank_id');
+            $insured->workplace_county_id = $request->input('Workplace_county_id');
+            $insured->birthplace_county_id = $request->input('Birthplace_county_id');
+            $insured->affiliation_status_id = $request->input('Affiliation_status_id');
+            $insured->county_id = $request->input('County_id');
+            $insured->file_number = Str::of($request->input('File_number'))->trim();
+            $insured->employee_number = Str::of($request->input('Employee_number'))->trim();
+            $insured->name = Str::of($request->input('Name'))->trim();
+            $insured->last_name_1 = Str::of($request->input('Last_name_1'))->trim();
+            $insured->last_name_2 = Str::of($request->input('Last_name_2'))->trim();
+            $insured->sex = $request->input('Sex');
+            $insured->birthday = $request->input('Birthday');
+            $insured->blood_type = $request->input('Blood_type');
+            $rfc = Str::of($request->input('Rfc'))->trim();
+            $insured->rfc = Str::upper($rfc);
+            $curp = Str::of($request->input('Curp'))->trim();
+            $insured->curp = Str::upper($curp) ?: null;
+            $insured->marital_status = $request->input('Marital_status');
+            $insured->phone = Str::of($request->input('Phone'))->trim() ?: null;
+            $email = Str::of($request->input('Email'))->trim();
+            $insured->email = Str::lower($email) ?: null;
+            $insured->neighborhood = Str::of($request->input('Neighborhood'))->trim() ?: null;
+            $insured->roadway_type = Str::of($request->input('Roadway_type'))->trim() ?: null;
+            $insured->street = Str::of($request->input('Street'))->trim() ?: null;
+            $insured->outdoor_number = Str::of($request->input('Outdoor_number'))->trim() ?: null;
+            $insured->interior_number = Str::of($request->input('Interior_number'))->trim() ?: null;
+            $insured->cp = Str::of($request->input('Cp'))->trim() ?: null;
+            $insured->locality = Str::of($request->input('Locality'))->trim() ?: null;
+            $insured->start_date = $request->input('Start_date');
+            $hoy = Carbon::today();
+            $insured->reentry_date = $hoy;
+            $insured->register_motive = Str::of($request->input('Register_motive'))->trim() ?: null;
+            $insured->observations = Str::of($request->input('Observations'))->trim() ?: null;
+            $insured->status = 'active';
+            $insured->modified_by = Auth::user()->email;
+            $insured->save();
+            DB::commit();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Registro guardado correctamente',
+                'insured' => $insured,
+            ], 201);
+        } catch (\Exception $e) {
             return response()->json([
                 'status' => 'fail',
                 'message' => 'Error en el servidor',
