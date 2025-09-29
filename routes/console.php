2178830e211c32b2669b28104ee22b5f49be693b
Trabajo <david_5343@hotmail.com>
2025-09-29 15:10:20 -0600

    agregando a la lista de comandos de Schedule run en clase console para comando: ExpireCredentialInsuredsCommand

--- routes/console.php
+++ routes/console.php
@@ -2,8 +2,7 @@
 
 use App\Console\Commands\BackupDatabase;
 use App\Console\Commands\FinalizePendingDeactivateCommand;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\ExpireCredentialInsuredsCommand;
 use Illuminate\Support\Facades\Schedule;
 
 // Artisan::command('inspire', function () {
@@ -11,5 +10,5 @@ use Illuminate\Support\Facades\Schedule;
 // })->purpose('Display an inspiring quote')->hourly();
 
 //Schedule::command(BackupDatabase::class)->dailyAt('19:00');
+Schedule::command(ExpireCredentialInsuredsCommand::class)->dailyAt('07:30');
 Schedule::command(FinalizePendingDeactivateCommand::class)->dailyAt('08:00');
-//Schedule::command(FinalizePendingDeactivateCommand::class)->everyMinute();
