373dce4331a272e8e5eb17f2c41b5fcffd26e418
Trabajo <david_5343@hotmail.com>
2025-10-09 22:32:08 -0600

    creando y agregando funcion para listar tipos de pension en funcion index dentro del controlador de tipo de pension en api

--- app/Http/Controllers/Api/SocioeconomicBenefits/PensionTypeApiController.php
+++ app/Http/Controllers/Api/SocioeconomicBenefits/PensionTypeApiController.php
@@ -3,16 +3,35 @@
 namespace App\Http\Controllers\Api\SocioeconomicBenefits;
 
 use App\Http\Controllers\Controller;
-use App\Models\SocioeconomicBenefits\PensionType;
+use App\Models\Catalogs\PensionType as CatalogsPensionType;
 use Illuminate\Http\Request;
 
 class PensionTypeApiController extends Controller
 {
     public function index()
     {
-        $tipoPensiones = PensionType::where('status', 'active')
-            ->get();
+        try {
 
-        return response()->json($tipoPensiones);
+            $pensions = CatalogsPensionType::all();
+
+            if ($pensions->isEmpty()) {
+                return response()->json([
+                    'status' => 'fail',
+                    'message' => 'Registro no encontrado',
+                    'pensions' => null,
+                ], 404);
+            }
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Búsqueda realizada correctamente',
+                'pensions' => $pensions,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'Error en el servidor',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
     }
 }
