1f2e08547dd0a74973930ddcaadd9aee6cafa450
David5343 <dsc.5343@gmail.com>
2025-10-09 23:25:14 -0600

    correccion en nombre de modelo de riesgo de trabajo

--- app/Models/Catalogs/WorkRisk.php
+++ app/Models/Catalogs/WorkRisk.php
@@ -5,7 +5,7 @@ namespace App\Models\Catalogs;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class WorkRisks extends Model
+class WorkRisk extends Model
 {
     public function pensionType(): BelongsTo
     {
