35272f3f55e808388768bcb730ec8bf3c5151712
Trabajo <david_5343@hotmail.com>
2025-10-20 16:05:18 -0600

    agregando relaciones a modelo de Pensioner

--- app/Models/SocioeconomicBenefits/Pensioner.php
+++ app/Models/SocioeconomicBenefits/Pensioner.php
@@ -2,9 +2,29 @@
 
 namespace App\Models\SocioeconomicBenefits;
 
+use App\Models\Catalogs\County;
+use App\Models\Catalogs\PensionType;
+use App\Models\Catalogs\Subdependency;
+use App\Models\Catalogs\WorkRisk;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Pensioner extends Model
 {
-    //
+    public function subdependency(): BelongsTo
+    {
+        return $this->belongsTo(Subdependency::class);
+    }
+    public function county(): BelongsTo
+    {
+        return $this->belongsTo(County::class, 'county_id');
+    }
+        public function pensionType(): BelongsTo
+    {
+        return $this->belongsTo(PensionType::class, 'pension_types_id');
+    }
+        public function workRisks(): BelongsTo
+    {
+        return $this->belongsTo(WorkRisk::class);
+    }
 }
