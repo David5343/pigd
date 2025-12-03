bf1715b6a30702105bbd2cd0afe36baa7e29cca8
David5343 <dsc.5343@gmail.com>
2025-12-02 23:54:10 -0600

    componente de vista de menu para  reportes

--- resources/views/livewire/socioeconomic-benefits/reports-menu.blade.php
+++ resources/views/livewire/socioeconomic-benefits/reports-menu.blade.php
@@ -1,6 +1,5 @@
 <div class="m-3">
     <livewire:messages />
-    <form wire:submit.prevent="guardar" class="flex flex-wrap gap-2">
         @if ($errors->any())
             <div class="w-full bg-red-100 text-red-700 dark:bg-red-900 dark:text-red-400 p-4 rounded-lg" role="alert">
                 <ul class="list-disc pl-5 space-y-2">
@@ -20,17 +19,16 @@
         <label class="text-sm font-medium text-gray-700">Reporte de:</label>
         <select wire:model="report_type" id="report_type" name="report_type"
             class="border rounded-lg px-3 py-2 w-full focus:ring focus:ring-blue-200" required>
-            <option value="">Altas</option>
-            <option value="">Bajas</option>
+            <option value="altas_titulares">Alta de titulares</option>
+            <option value="bajas_titulares">Baja de titulares</option>
             <option value="">Preafiliados</option>
         </select>
-
         @error('report_type')
             <div class="bg-red-100 text-red-700 p-2 rounded-lg mt-2">{{ $message }}</div>
         @enderror
     </div>
 
-    <!-- Tipo de derechohabiente -->
+    {{-- <!-- Tipo de derechohabiente -->
     <div class="flex flex-col w-full md:w-1/5">
         <label class="text-sm font-medium text-gray-700">Tipo de derechohabiente</label>
         <select wire:model="insured_type" id="insured_type" name="insured_type"
@@ -40,11 +38,10 @@
             <option value="">Pensionados</option>
             <option value="">Beneficiarios</option>
         </select>
-
         @error('insured_type')
             <div class="bg-red-100 text-red-700 p-2 rounded-lg mt-2">{{ $message }}</div>
         @enderror
-    </div>
+    </div> --}}
 
 </div>
 <div class="flex flex-wrap w-full gap-4 mt-4">
@@ -52,33 +49,28 @@
     <!-- Fecha inicio -->
     <div class="flex flex-col w-full md:w-1/4">
         <label class="text-sm font-medium text-gray-700">Fecha inicio</label>
-        <input wire:model="effective_date_start" type="date" id="effective_date_start" name="effective_date_start"
+        <input wire:model="date_start" type="date" id="date_start" name="date_start"
             class="border rounded-lg px-3 py-2 w-full focus:ring focus:ring-blue-200" required>
-
-        @error('effective_date_start')
+        @error('date_start')
             <div class="bg-red-100 text-red-700 p-2 rounded-lg mt-2">{{ $message }}</div>
         @enderror
     </div>
-
     <!-- Fecha final -->
     <div class="flex flex-col w-full md:w-1/4">
         <label class="text-sm font-medium text-gray-700">Fecha final</label>
-        <input wire:model="effective_date_end" type="date" id="effective_date_end" name="effective_date_end"
+        <input wire:model="date_end" type="date" id="date_end" name="date_end"
             class="border rounded-lg px-3 py-2 w-full focus:ring focus:ring-blue-200" required>
-
-        @error('effective_date_end')
+        @error('date_end')
             <div class="bg-red-100 text-red-700 p-2 rounded-lg mt-2">{{ $message }}</div>
         @enderror
     </div>
-
 </div>
         <div class="w-full flex justify-end gap-3 m-4">
             <a href="{{ route('banks.index') }}"
                 class="bg-gray-300 text-gray-700 px-4 py-2 rounded-lg hover:bg-gray-400 transition">
                 Cancelar
             </a>
-            <button type="submit"
+            <button wire:click="generarPDFTitularAltas"
                 class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">Generar</button>
         </div>
-    </form>
 </div>
