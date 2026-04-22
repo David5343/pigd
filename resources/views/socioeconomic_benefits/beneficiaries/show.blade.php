e240f37a32ba688b22a3b1469ec3892737a1ec2c
Sacd2023 <david_5343@hotmail.com>
2026-04-22 17:51:22 -0600

    terminando visualizacion de fotos en asegurados y familiares

--- resources/views/socioeconomic_benefits/beneficiaries/show.blade.php
+++ resources/views/socioeconomic_benefits/beneficiaries/show.blade.php
@@ -21,7 +21,7 @@
                 </div>
                 <div class="flex justify-center mt-5">
                     <figure class="mb-4 inline-block max-w-sm">
-                        <img src="{{ empty($familiar->photo) ? asset('images/icono_no_imagen.png') : url('/photo/beneficiaries' . $familiar->file_number.'.jpg') }}"
+                        <img src="{{ empty($familiar->photo) ? asset('images/icono_no_imagen.png') : url('/photo/beneficiaries/' . $familiar->file_number.'.jpg') }}"
                         class="mb-4 h-auto max-w-full max-h-full rounded-lg align-middle leading-none shadow-lg">
                         <figcaption class="text-center text-sm text-neutral-600 dark:text-neutral-400">
                             <p><span>ESTATUS DE AFILIACIÓN</span></p>
--- resources/views/socioeconomic_benefits/membership/show.blade.php
+++ resources/views/socioeconomic_benefits/membership/show.blade.php
@@ -20,7 +20,7 @@
                 </div>
                 <div class="flex justify-center mt-5">
                     <figure class="mb-4 inline-block max-w-sm">
-                        <img src="{{ empty($titular->photo) ? asset('images/icono_no_imagen.png') : url('/photo/insureds' . $titular->file_number.'.jpg') }}"
+                        <img src="{{ empty($titular->photo) ? asset('images/icono_no_imagen.png') : url('/photo/insureds/' . $titular->file_number.'.jpg') }}"
                         class="mb-4 h-auto max-w-full max-h-full rounded-lg align-middle leading-none shadow-lg">
                         <figcaption class="text-center text-sm text-neutral-600 dark:text-neutral-400">
                             <p><span>ESTATUS DE AFILIACIÓN</span></p>
